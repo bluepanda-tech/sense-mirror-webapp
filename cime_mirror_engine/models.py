df9e87c57c0a9da1cf86cc34cb9fb6447d88a4f4
Sebastian Arboleda <sebasarboleda22@gmail.com>
2018-11-05 22:35:23 -0500

    Re-structured models and resources' validations

--- cime_mirror_engine/models.py
+++ cime_mirror_engine/models.py
@@ -1,14 +1,6 @@
-import os
-import uuid
-
-from werkzeug.utils import secure_filename
-
-from .config import MAX_PRODUCTS, MAX_STORED_MEDIA_FILES, BASE_MEDIA_DIR
+from .config import ALLOWED_FILE_EXTENSIONS
 from .app import db
-from .utils import (
-    file_type,
-    allowed_product_id,
-)
+from .utils import allowed_file, allowed_product_id
 
 class Product(db.Model):
     """Products to be showcased by the UI"""
@@ -19,29 +11,21 @@ class Product(db.Model):
     description_txt = db.Column(db.Text)
     media_files = db.relationship('MediaFile', backref='product', lazy=True)
     thumbnail = db.Column(db.Text, default=None) # Name of thumbnail file
-    is_displayed = db.Column(db.Boolean, nullable=False, default=False)
+    is_displayed = db.Column(db.Boolean, nullable=False, default=True)
 
     def __init__(self, product_id):
-        if allowed_product_id(product_id, MAX_PRODUCTS) and not Product.product_exists(product_id):
+        if allowed_product_id(product_id) and not Product.product_exists(product_id):
             self.product_id = product_id
             self.name = "Item {}".format(product_id)
-        elif Product.product_exists(product_id):
-            #TODO test this
-            raise ValueError('Item {} already exists'.format(product_id))
         else:
-            raise ValueError('Invalid item number', product_id)
+            raise ValueError('Product id not allowed')
 
-    def add_thumbnail(self, file):
-        """Updates the name of the file. If a file was already uploaded, the function will
-        first add they new path and update it and then remove the old file"""
-        filename = secure_filename("{}.{}".format(str(uuid.uuid4())[:8], file_type(file.filename)))
-        file_path = os.path.join(BASE_MEDIA_DIR, filename)
-        old_filename = self.thumbnail
-        file.save(file_path)
-        self.thumbnail = filename
-        #TODO add an event to tell the UI to look for a new thumbnail
-        if old_filename is not None:
-            os.remove(os.path.join(BASE_MEDIA_DIR, old_filename))
+    def add_thumbnail_filename(self, filename):
+        """Updates the name of the file"""
+        if allowed_file(filename, ALLOWED_FILE_EXTENSIONS):
+            self.thumbnail = filename
+        else: 
+            raise ValueError('Thumbnail filename not allowed')
 
     def product_exists(product_id):
         """Checks if the item already exists based on the item number"""
@@ -54,20 +38,16 @@ class Product(db.Model):
 class MediaFile(db.Model):
     """Paths to media files to be displayed in the product's description"""
     __tablename__ = 'media_files'
-    
+
     filename = db.Column(db.Text, primary_key=True, nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey('products.product_id'), nullable=False)
 
-    def __init__(self, product_id, file):
-        #TODO add file validation in the resources file...
-        if MediaFile.query.filter_by(product_id=product_id).count() < MAX_STORED_MEDIA_FILES:
-            filename = secure_filename("{}.{}".format(str(uuid.uuid4())[:8], file_type(file.filename)))
-            file_path = os.path.join(BASE_MEDIA_DIR, filename)
-            file.save(file_path)
+    def __init__(self, product_id, filename):
+        if allowed_file(filename, ALLOWED_FILE_EXTENSIONS):
             self.product_id = product_id
             self.filename = filename
         else:
-            raise ValueError('Reached Max Number of Media files for this product')
+            raise ValueError('Filename not allowed: "{}"'.format(filename))
     
     def __repr__(self):
         return "{}".format(self.filename)
--- cime_mirror_engine/resources.py
+++ cime_mirror_engine/resources.py
@@ -1,11 +1,16 @@
-import os
+import os, uuid
 
 from flask import request
 from flask_restful import Resource
+from werkzeug.utils import secure_filename
 
 from .app import app, api, db
 from .models import Product, MediaFile
-from .config import BASE_MEDIA_DIR, ALLOWED_FILE_EXTENSIONS
+from .config import (
+    BASE_MEDIA_DIR,
+    ALLOWED_FILE_EXTENSIONS,
+    MAX_STORED_MEDIA_FILES,
+)
 from .utils import (
     file_type,
     allowed_file,
@@ -15,39 +20,55 @@ class UploadThumbnail(Resource):
     """Upload either a video or an image file to be used as the product thumbnail"""
         
     def post(self, product_id):
+        """API endpoint to upload a product's thumbnail"""
+
+        #TODO log this and better error handling
+
         file = request.files['file']
+
         if file and allowed_file(file.filename, ALLOWED_FILE_EXTENSIONS):
             product = Product.query.get(product_id)
-            product.add_thumbnail(file)
+            old_filename = product.thumbnail
+            filename = secure_filename("{}.{}".format(str(uuid.uuid4())[:8], file_type(file.filename)))
+            file.save(os.path.join(BASE_MEDIA_DIR, filename))
+            product.add_thumbnail_filename(filename)
             db.session.add(product)
             db.session.commit()
+            #TODO add an event to tell the UI to look for a new thumbnail, so that the command above is
+            #executed only when the UI sends an event indicating it is now aware of the change
+            if old_filename is not None:
+                os.remove(os.path.join(BASE_MEDIA_DIR, old_filename))
             return "File Uploaded"
+        elif not allowed_file(file.filename, ALLOWED_FILE_EXTENSIONS):
+            return 'File not allowed. Upload only {} files'.format([ftype for ftype in ALLOWED_FILE_EXTENSIONS])
+        elif Product.product_exists(product_id):
+            return 'Item {} already exists'.format(product_id)
         else:
-            #TODO log this and better error handling
-            return 'File not allowed'
-api.add_resource(UploadThumbnail, '/api/upload/thumbnail/<string:product_id>')
+            return 'Invalid item number: {}'.format(product_id)
+api.add_resource(UploadThumbnail, '/api/thumbnail/<string:product_id>')
 
-class UploadMediaFile(Resource):
-    """Upload media files to be shown in the product's description"""
+class MediaFiles(Resource):
+    """Upload or delete media files to be shown in the product's description"""
 
     def post(self, product_id):
         """Upload a mediafile"""
         file = request.files['file']
-        if file and allowed_file(file.filename, ALLOWED_FILE_EXTENSIONS):
-            try:
-                new_file = MediaFile(product_id, file)
-                db.session.add(new_file)
-                db.session.commit()
-                return "File Uploaded"
-            except Exception as e:
-                return e
-        else:
+        if file and allowed_file(file.filename, ALLOWED_FILE_EXTENSIONS) and MediaFile.query.filter_by(product_id=product_id).count() < MAX_STORED_MEDIA_FILES and Product.product_exists(product_id):
+            # file must be allowed, product_id must exist and media files should be less than max allowed
             #TODO log this and better error handling
-            return 'File not allowed'
-api.add_resource(UploadMediaFile, '/api/upload/mediafile/<string:product_id>')
-
-class DeleteMediaFile(Resource):
-    """Deletes mediafile"""
+            filename = secure_filename("{}.{}".format(str(uuid.uuid4())[:8], file_type(file.filename)))
+            file_path = os.path.join(BASE_MEDIA_DIR, filename)
+            file.save(file_path)
+            new_file = MediaFile(product_id, filename)
+            db.session.add(new_file)
+            db.session.commit()
+            return "File Uploaded"
+        elif not allowed_file(file.filename, ALLOWED_FILE_EXTENSIONS):
+            return "File not allowed, must be of format {}".format([filetype for filetype in ALLOWED_FILE_EXTENSIONS])
+        elif not MediaFile.query.filter_by(product_id=product_id).count() < MAX_STORED_MEDIA_FILES:
+            return "Product reached max number of mediafiles: {}".format(MAX_STORED_MEDIA_FILES)
+        elif not Product.product_exists(product_id):
+            return "Product ID is invalid."
 
     def delete(self, filename):
         """Delete a media file"""
@@ -59,4 +80,4 @@ class DeleteMediaFile(Resource):
             return "File Deleted"
         else:
             return "File doesn't exist"
-api.add_resource(DeleteMediaFile, '/api/delete/mediafile/<string:filename>')
+api.add_resource(MediaFiles, '/api/mediafile/<string:product_id>')
